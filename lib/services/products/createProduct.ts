8e3f9250eb75366fd372cf5fcd4e137961dbec8c
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-06-24 00:34:32 -0400

    implement transaction based creation

--- lib/services/products/createProduct.ts
+++ lib/services/products/createProduct.ts
@@ -1,7 +1,14 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { headers } from '../configs/headers.config';
-import { putItemToDB } from '../../utils/putItemToDb';
 import { v4 as uuidv4 } from 'uuid';
+import * as AWS from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  TransactWriteCommand,
+} from '@aws-sdk/lib-dynamodb';
+import { headers } from '../configs/headers.config';
+
+const dbClient = new AWS.DynamoDB();
+const ddbDocClient = DynamoDBDocumentClient.from(dbClient);
 
 const handler = async (event: APIGatewayProxyEvent) => {
   try {
@@ -30,11 +37,24 @@ const handler = async (event: APIGatewayProxyEvent) => {
       };
     }
 
-    await putItemToDB(process.env.productsTableName, {
-      id,
-      ...product,
-    });
-    await putItemToDB(process.env.stocksTableName, { product_id: id, count });
+    await ddbDocClient.send(
+      new TransactWriteCommand({
+        TransactItems: [
+          {
+            Put: {
+              TableName: process.env.productsTableName,
+              Item: { id, ...product },
+            },
+          },
+          {
+            Put: {
+              TableName: process.env.stocksTableName,
+              Item: { product_id: id, count },
+            },
+          },
+        ],
+      })
+    );
 
     return {
       statusCode: 200,
