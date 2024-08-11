dd2084e72ded5b5d083da36d83fd5553cb913d59
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-08-10 21:31:26 -0400

    set bootstrap

--- services/bff-service/src/main.ts
+++ services/bff-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+
+  app.use(helmet());
+
+  await app.listen(port);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log('App is running on %s port', port);
+});
