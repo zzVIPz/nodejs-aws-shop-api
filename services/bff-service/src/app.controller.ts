6f888b5f6602f2f9395541a367b3c21489a6a0dd
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-08-11 22:03:35 -0400

    integrate product service

--- services/bff-service/src/app.controller.ts
+++ services/bff-service/src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  All,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get(['', 'ping'])
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @All([':recipientServiceName/*', ':recipientServiceName'])
+  async redirectToService(
+    @Param('recipientServiceName') recipientServiceName: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const recipientURL = process.env[recipientServiceName.toLocaleUpperCase()];
+
+    if (!recipientURL)
+      throw new HttpException('Cannot process request', HttpStatus.BAD_GATEWAY);
+
+    const { status, data } = await this.appService.redirectToService(
+      recipientURL,
+      req,
+    );
+
+    return res.status(status).send(data);
   }
 }
--- services/bff-service/src/app.service.ts
+++ services/bff-service/src/app.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Hello from BFF',
+    };
+  }
+
+  async redirectToService(recipientURL: string, req: Request): Promise<any> {
+    console.log('recipientURL', recipientURL);
+
+    const { method, body } = req;
+
+    try {
+      const res = await axios({
+        method,
+        data: body,
+        baseURL: recipientURL,
+      });
+
+      console.log('res', res.data);
+
+      return res;
+    } catch ({ response }) {
+      if (response) return response;
+
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
